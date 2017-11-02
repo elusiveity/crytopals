f02bf56caab7c507dd53858df0f9582b77ac6fc8
elusiveity <elusive.ity@gmail.com>
2017-11-02 03:55:39 +0000

    Read me added because this is a public repo. xor started, but it sucks.

--- 1-basics/2-fixed-xor/xor.py
+++ 1-basics/2-fixed-xor/xor.py
@@ -4,11 +4,8 @@ thing1 = "1c0111001f010100061a024b53535009181c"
 thing2 = "686974207468652062756c6c277320657965"
 
 def fixedxor(a,b):
-	c = a.decode('hex')
-	d = b.decode('hex')
-	if bool(c) ^ bool(d):
-		print "xored"
-	else:
-		print "Not xored"
+
+	for c,d in a,b:
+		print c + ":" + d
 
 fixedxor(thing1,thing2)
---
+++ README.md
@@ -0,0 +1,9 @@
+# Welcome to my thingo
+
+You're going to see a lot of words being used in this code that might cause personal offence to you. I don't care. Grow the fuckup; this is the internet.
+
+# Get Involved
+
+Want to do this yourself? Head over to [https://cryptopals.com/]
+
+
